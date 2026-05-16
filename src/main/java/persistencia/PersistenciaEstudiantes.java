f97bccf6e1444de11652e1e046dd03fb48804b47
JorgeU5 <jorge.urbina262725@potros.itson.edu.mx>
2026-05-16 12:12:29 -0700

    Update PersistenciaEstudiantes.java

--- src/main/java/persistencia/PersistenciaEstudiantes.java
+++ src/main/java/persistencia/PersistenciaEstudiantes.java
@@ -14,12 +14,26 @@ import excepciones.PersistenciaException;
  * @author Jorge
  */
 public class PersistenciaEstudiantes {
+    /**
+     * diccionario principal
+     * llave = matricula
+     */
     private DiccionarioTablaHash<String, Estudiante> estudiantes;
-
+    
+    /**
+     * constructor que inicializa la estructura con capacidad de 100 estudiantes
+     */
     public PersistenciaEstudiantes() {
         estudiantes = new DiccionarioTablaHash<>(100);
     }
 
+    /**
+     * registra un nuevo estudiante
+     * valida que no sea null y que la matricula no exista
+     *
+     * @param estudiante estudiante a registrar
+     * @throws PersistenciaException si el estudiante es null o ya existe
+     */
     public void registrarEstudiante(Estudiante estudiante) throws PersistenciaException {
         if (estudiante == null) {
             throw new PersistenciaException("El estudiante no puede ser null");
@@ -32,6 +46,13 @@ public class PersistenciaEstudiantes {
         estudiantes.put(estudiante.getMatricula(), estudiante);
     }
 
+    /**
+     * busca un estudiante por su matricula
+     *
+     * @param matricula matricula del estudiante
+     * @return estudiante encontrado
+     * @throws PersistenciaException si no existe el estudiante con esa matricula
+     */
     public Estudiante buscarEstudiante(String matricula) throws PersistenciaException {
         try {
             return estudiantes.get(matricula);
@@ -40,6 +61,12 @@ public class PersistenciaEstudiantes {
         }
     }
 
+    /**
+     * elimina un estudiante usando su matricula
+     *
+     * @param matricula matricula del estudiante a eliminar
+     * @throws PersistenciaException si el estudiante no existe o no puede eliminarse
+     */
     public void eliminarEstudiante(String matricula) throws PersistenciaException {
         try {
             estudiantes.remove(matricula);
@@ -48,10 +75,20 @@ public class PersistenciaEstudiantes {
         }
     }
 
+     /**
+     * obtiene todos los estudiantes registrados
+     *
+     * @return lista enlazada con todos los estudiantes
+     */
     public ListaEnlazada<Estudiante> obtenerTodos() {
         return estudiantes.obtenerTodosLosValores();
     }
 
+    /**
+     * genera una lista de estudiantes ordenados de mayor a menor promedio
+     *
+     * @return lista enlazada ordenada por promedio
+     */
     public ListaEnlazada<Estudiante> listarOrdenadosPorPromedio() {
         ListaEnlazada<Estudiante> listaOriginal = estudiantes.obtenerTodosLosValores();
         ListaEnlazada<Estudiante> ordenados = new ListaEnlazada<>();
